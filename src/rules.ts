d84b14b8e3355df6e9f431d62850a9e0be083279
Manabu Nakazawa <mshibanami+git@gmail.com>
2025-10-31 13:12:27 +1100

    Resolve type errors

--- src/rules.ts
+++ src/rules.ts
@@ -163,7 +163,7 @@ export class Rules {
   }
 }
 
-function findRule(rules: Rule[], node: Node, options: TurnishOptions): Rule | undefined {
+function findRule(rules: Rule[], node: ExtendedNode, options: TurnishOptions): Rule | undefined {
   for (let i = 0; i < rules.length; i++) {
     const rule = rules[i];
     if (filterValue(rule, node, options)) return rule;
@@ -171,15 +171,22 @@ function findRule(rules: Rule[], node: Node, options: TurnishOptions): Rule | un
   return undefined;
 }
 
-function filterValue(rule: Rule, node: Node, options: TurnishOptions): boolean {
-  const filter = rule.filter
+function filterValue(rule: Rule, node: ExtendedNode, options: TurnishOptions): boolean {
+  const filter = rule.filter;
   if (typeof filter === 'string') {
-    if (filter === node.nodeName.toLowerCase()) return true
+    if (filter === node.nodeName.toLowerCase()) {
+      return true;
+    }
   } else if (Array.isArray(filter)) {
-    if (filter.indexOf(node.nodeName.toLowerCase()) > -1) return true
+    if (filter.indexOf(node.nodeName.toLowerCase()) > -1) {
+      return true;
+    }
   } else if (typeof filter === 'function') {
-    if (filter.call(rule, node, options)) return true
+    if (filter(node, options)) {
+      return true;
+    }
   } else {
-    throw new TypeError('`filter` needs to be a string, array, or function')
+    throw new TypeError('`filter` needs to be a string, array, or function');
   }
+  return false;
 }
